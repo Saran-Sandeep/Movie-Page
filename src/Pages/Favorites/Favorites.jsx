d100b131fd0be8fc69af645e84438a25cc00ae9f
Saran Sandeep <saransandeepyedla03@gmail.com>
2025-11-08 15:40:47 +0530

    Added Favorites Page

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -1,3 +1,28 @@
+import "./Favorites.css";
+import { useMovieContext } from "../../contexts/useMovieContext";
+import MovieCard from "../../Components/MovieCard/MovieCard";
+
 export default function Favorites() {
-  return <div>Favorites</div>;
+  const { favorites } = useMovieContext();
+  const hasFavorites = favorites && favorites.length > 0;
+
+  return (
+    <section className="favorites container">
+      <h2 className="favorites__title">Your Favorites</h2>
+
+      {hasFavorites ? (
+        <div className="card__container">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      ) : (
+        <div className="favorites__empty">
+          <div className="favorites__icon">🍿</div>
+          <h3>No Favorite Movies Yet</h3>
+          <p>Start adding movies to your favorites and they’ll appear here!</p>
+        </div>
+      )}
+    </section>
+  );
 }
