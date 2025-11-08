f18d2f3c53c4694163fcf0785483ee970146de49
Saran Sandeep <saransandeepyedla03@gmail.com>
2025-11-08 14:15:25 +0530

    Added global css imports in main.jsx file.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./globalCSS/modern-normalize.css";
+import "./globalCSS/utils.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
